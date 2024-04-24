a1583e0c9b9e4d58796b9910e1805685829e4e46
Lucas Sylvain <2263248@csfoy.ca>
2024-04-24 17:48:26 -0400

    push ajout index.php

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div>
+        <a class="navbar-brand" href="#">Place holder</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="accordion">
+                    <div class="accordion-item">
+                        <h1 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Accordion Item #1
+                            </button>
+                        </h1>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
