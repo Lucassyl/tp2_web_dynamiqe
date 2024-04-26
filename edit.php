6ebf06c1a645a2540b6232e1eeb070cfb7e42ad7
cedric <cedric.doyon2005@gmail.com>
2024-04-26 11:31:23 -0400

    Ajout des dernière pages php importantes + ajout important pour la page signin.php

--- edit.php
+++ edit.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des pages</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <title>Edit page</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <script src="js/bootstrap.min.js" defer></script>
     <script defer src="js/code.js"></script>
 </head>
-
 <body>
     
+    
+
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -6,14 +6,45 @@
     <title>Page de connexion</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
+
     <script src="js/bootstrap.min.js" defer></script>
     <script defer src="js/code.js"></script>
 
 </head>
 <body>
     
-    
+    <?php include_once('includes/header.php'); ?>
 
+    <main>
+
+        <form method="post" action="signin.php" novalidate>
+            <fieldset class="conteneur-login">
+                <div class="login">
+                    <label for="txtLogin" class="labelLogin">Login</label>
+                    <input type="text" class="inputLogin" id="txtLogin" name="txtLogin"
+                    <?php
+                    if (isset($_POST['txtLogin']))
+                    {
+                        echo 'value="' . $_POST['txtLogin'] . '"';
+                    }
+                    ?>>
+                </div>
+                <div class="password">
+                    <label for="txtPassword" class="labelPassword">Mot de passe</label>
+                    <input type="text" class="inputPassword" id="txtPassword" name="txtPassword"
+                    <?php
+                    if (isset($_POST['txtPassword']))
+                    {
+                        echo 'value="' . $_POST['txtPassword'] . '"';
+                    }
+                    ?>>
+                </div>
+            </fieldset>
+        </form>
+
+    </main>
+
+    <?php include_once('includes/footer.html'); ?>
+    
 </body>
 </html>
