36a46c7d8f6668d95c18ba6251ef89a99a6016aa
Lucas Sylvain <2263248@csfoy.ca>
2024-05-10 12:09:12 -0400

    push merge conflict delete page
    mise a jour de la page delete pour un merge conflict

--- delete.php
+++ delete.php
@@ -17,7 +17,31 @@
         <div class="d-flex">
             <div class="row rounded alert alert-warning border-3 mt-5 justify-content-center">
                 <?php
-                
+                if(!isset($_GET["id"])){
+                    header('Location:list.php');
+                }else{
+                    $pages = new pagesDAO($conn);
+                    if(isset($_POST["Confirmation"])){
+                        //uncomment before submiting 
+                        //$pages->delete($_GET["id"]);
+                        //header('Location:list.php');
+                        echo 'deleted';
+                    }
+                    $page = $pages->getPageById($_GET["id"]);
+                ?>
+                <p class="text-center display-5"><strong>Attention!!!</strong></p>
+                <p class="text-center">Vous êtes sur le point de supprimer la page <?php echo $page->getNomMenu(); ?>.</p>
+                <p class="text-center"><small><u>Ce changement est permanent et n'est pas réversible!</u></small></p>
+                <p class="text-center">Êtes vous sûr de vouloir continuer?</p>
+                <div class="col-4">
+                    <button class="bouton-edit-list"><a href="list.php?id=<?php echo $_GET["id"]; ?>" class="lien-edit-list">Cancel</a></button>
+                </div>
+                <form class="col-4" action="delete.php?id=<?php echo $_GET["id"]; ?>" method="post">
+                    <input class="bouton-delete-list lien-edit-list" type="submit" name="Confirmation" value="Continue" />
+                </form>
+                <?php
+                    }
+                ?>
             </div>
         </div>
     </div>
