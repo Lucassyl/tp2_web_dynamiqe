4a5be55d3e69423b0cecc5a5754d250738b49e1e
cedric <cedric.doyon2005@gmail.com>
2024-05-18 10:24:26 -0400

    Ajout d'un commentaire

--- includes/logoutPage.php
+++ includes/logoutPage.php
@@ -3,7 +3,7 @@ session_start();
 session_destroy();
 echo "Déconnexion en cours";
 
-//Code trouvé pour l'attente sur: https://stackoverflow.com/questions/1901796/redirect-with-timer-in-php
+//Code pour la redirection avec un timer en php trouvé sur: https://stackoverflow.com/questions/1901796/redirect-with-timer-in-php
 header("refresh:1; url=../index.php");
 exit;
 ?>
